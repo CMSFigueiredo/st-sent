490d86fa8f9f2ab85e70f6b516972ecfcda5f894
Figueiredo <cfigueiredo@users.noreply.huggingface.co>
2024-05-07 15:38:09 +0000

    update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from transformers import pipeline
 
-pipe = pipeline('sentiment-analysis:)
-text = st.text_area("Entre com a frase:")
+pipe = pipeline('sentiment-analysis:')
+text = st.text_area('Entre com a frase:')
 
 if text:
   out=pipe(text)
